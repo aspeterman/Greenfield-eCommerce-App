13514444a152ad1ca20a0c441580530516a1b015
Sterling290 <sterling.becvar@gmail.com>
2020-01-24 18:51:19 -0700

    trying to fix webpack, messing with some npm and node modules

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import './App.css';
+import Aq_Main from './Questions_Answers/main_QA/Aq_Main.jsx'
 // import NavBar from './actions/overall/NavBar.jsx'
 
 class App extends Component {
@@ -8,6 +9,7 @@ class App extends Component {
   return (
     <div className="App">
       {/* <NavBar /> */}
+      <Aq_Main/>
       <header className="App-header">
       </header>
     </div>
