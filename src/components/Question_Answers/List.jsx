8604751b10cb0e0e4b135bc0959188b9e0325685
Sterling290 <sterling.becvar@gmail.com>
2020-02-03 11:06:43 -0700

    Answer images for question and answers working

--- src/components/Question_Answers/List.jsx
+++ src/components/Question_Answers/List.jsx
@@ -26,6 +26,15 @@ class List extends Component {
               <ul id='answer'>
               {Object.keys(each["answers"]).map((every,i) =>
                <li key ={i}>{`A: ${each["answers"][every]["body"]}`}<br></br>
+              {console.log(each["answers"][every]['photos'])}
+              {each["answers"][every]['photos'].length > 0?each["answers"][every]['photos'].map((photo,i)=>
+              <ul>
+                {console.log("photo:",photo)}
+              {/* <li key ={i}><image  src={photo} alt="Smiley face" height="42" width="42"></image>/></li> */}
+              <li key ={i}><img src = {photo} alt="Smiley face" height="42" width="42"></img></li>
+              </ul>
+              ):console.log('false')}
+
               {`by${each["answers"][every]["date"]},${each["answers"][every]["answerer_name"]}| helpful? `}<a href="/">yes</a>{`(${each["answers"][every]["helpfulness"]})|`}<a href="#">Report</a><br></br></li>
               )}
               <a href="#">Load more answers</a><br></br>
